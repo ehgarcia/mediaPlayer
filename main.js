1a07f667b77049f2eb6d5871ec706d7831ea1b9e
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-02 12:43:55 -0300

    agrego estilos al video

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const $video = document.querySelector('#video')
 
 $play.addEventListener('click', handlePlay)
 $pause.addEventListener('click', handlePause)
+$backward.addEventListener('click', handleBackward)
+$forward.addEventListener('click', handleForward)
 
 function handlePlay (){
     $video.play()
@@ -19,4 +21,14 @@ function handlePause (){
     $video.pause()
     $play.hidden = false
     $pause.hidden = true
+}
+
+function handleBackward(){
+    $video.currentTime -= 10
+    console.log($video.currentTime)
+}
+
+function handleForward (){
+    $video.currentTime += 10
+    console.log($video.currentTime)
 }

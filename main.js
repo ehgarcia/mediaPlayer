ad8b97154d6cbf6a98ddc2f07b82c682a2f98280
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-02 11:58:39 -0300

    acomode el orden de los botones

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ const $forward = document.querySelector('#forward')
 const $video = document.querySelector('#video')
 
 $play.addEventListener('click', handlePlay)
+$pause.addEventListener('click', handlePause)
 
 function handlePlay (){
-    
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+}
+
+function handlePause (){
+    $video.pause()
+    $play.hidden = false
+    $pause.hidden = true
 }

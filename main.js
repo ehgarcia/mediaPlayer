3f667e551ec6656e5c8bca1827b9ee1cf8bdaa37
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-02 11:43:48 -0300

    se agregan estilos y funciones

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+const $backward = document.querySelector('#backward')
+const $forward = document.querySelector('#forward')
+const $video = document.querySelector('#video')
+
+$play.addEventListener('click', handlePlay)
+
+function handlePlay (){
+    
+}
